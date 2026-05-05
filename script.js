7f28237c2fc3b6497b63c3ed313e527bd20a3cdf
Sagarmogli8976 <114072291+Sagarmogli8976@users.noreply.github.com>
2026-05-05 14:29:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-// complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Method for Person
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+function Employee(name, age, jobTitle) {
+  // inherit properties
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// inherit methods
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+// Method for Employee
+Employee.prototype.jobGreet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
